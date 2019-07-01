11774e4f0f0b6c0ee3ab1d61c98b94ca45c61832
Gaurav Tyagi <gtyagi88cse@gmail.com>
2019-07-02 03:04:46 +0530

    Different Ways To Implement OnClickListener On Android

--- app/src/main/java/com/example/buttonclicks/MainActivity.kt
+++ app/src/main/java/com/example/buttonclicks/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     val TAG = "BTN"
 
@@ -14,15 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn1.setOnClickListener {
+        btn2.setOnClickListener {
             Log.d(TAG, "Second Button Clicked")
         }
 
+        btn3.setOnClickListener(this)
+
     }
 
-    fun onButtonClicked (v: View)
-    {
+    fun onButtonClicked (v: View) {
         Log.d(TAG, "Button Clicked")
+    }
+
+    override fun onClick(p0: View?) {
+        Log.d(TAG, "3rd Button Clicked")
     }
 
 }
